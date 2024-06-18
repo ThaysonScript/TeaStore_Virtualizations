769e36c10add5fea9c0595f2d82836d701665e78
root <root@thayson-pc.intelbras.local>
2024-06-18 16:41:47 -0300

    rm

--- generals/root/User/History/-4854b31d/AMJl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-array=("tomcat-auth" "tomcat-webui" "tomcat-image")
-
-for indice in ${@array}; do
-
-    echo $indice
-
-done
--- generals/root/User/History/28bce481/W8LR.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-java -jar $CATALINA_HOME/$index/bin/dockermemoryconfigurator.jar ${TOMCAT_HEAP_MEM_PERCENTAGE};
-bash $CATALINA_HOME/$index/bin/start.sh $CATALINA_HOME/$index
-bash $CATALINA_HOME/$index/bin/catalina.sh run
